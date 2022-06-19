2727512c3a250ceef76893e2c194c13e0bc57649
pooreumu <pooreum83@gmail.com>
2022-06-19 20:03:33 +0900

    feat(master) policies.moudule에 co, providers, improts  추가

--- src/policies/policies.module.ts
+++ src/policies/policies.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PoliciesController } from './policies.controller';
+import { PoliciesService } from './policies.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature()],
+  controllers: [PoliciesController],
+  providers: [PoliciesService],
+})
 export class PoliciesModule {}
