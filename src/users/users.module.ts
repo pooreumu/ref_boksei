9bb8ea68064b6657b4d17dbd2b36ba0dca6e1ba2
pooreumu <pooreum83@gmail.com>
2022-06-20 21:23:04 +0900

    feat(master)users.module 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
-@Module({})
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    TypeOrmModule.forFeature(),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
 export class UsersModule {}
